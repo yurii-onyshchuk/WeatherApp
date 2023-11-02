a645b93c0f3bed00fd1971f3ef2bf60a0e599abb
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2023-11-02 20:43:03 +0200

    Added model to represent weather data

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class WeatherData(models.Model):
+    """Model to represent weather data for a specific city and date."""
+
+    city = models.CharField(verbose_name='Місто', max_length=100)
+    date = models.DateField(verbose_name='Дата спостереження')
+    temperature = models.DecimalField(verbose_name='Температура', max_digits=3, decimal_places=1)
