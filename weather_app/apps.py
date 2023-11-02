434a861b72dce7e77b561b72e0112393c8487853
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2023-11-02 20:29:25 +0200

    Initial commit

---
+++ weather_app/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class WeatherAppConfig(AppConfig):
+    """Configuration class for the 'weather_app' app."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'weather_app'
